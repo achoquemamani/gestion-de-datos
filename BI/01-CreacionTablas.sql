08c685d80c720951f16bbbea19536dfee31e632f
Matías Otermín <mati_otermin@yahoo.com>
2022-11-11 17:01:50 -0300

    Borrado y creación de tablas, PKs y FKs para BI_PROVINCIA y BI_HECHO_VENTA_PRODUCTO

---
+++ BI/01-CreacionTablas.sql
@@ -0,0 +1,21 @@
+CREATE TABLE HARAKIRI.BI_HECHO_VENTA_PRODUCTO(
+    VENTA_PRODUCTO_CODIGO decimal(19,0) not null,
+    EDAD_CODIGO decimal(19,0),
+    TIEMPO_CODIGO decimal(19,0),
+    PROVINCIA_CODIGO decimal(18,0),
+    CANAL_CODIGO decimal(19,0),
+    MEDIO_DE_PAGO_CODIGO decimal(19,0),
+    CATEGORIA_CODIGO decimal(18,0),
+    PRODUCTO_CODIGO nvarchar(50),
+    MEDIO_ENVIO_CODIGO decimal(19,0),
+    VENTA_ENVIO_PRECIO decimal(18,2),
+    PRODUCTO_PRECIO_TOTAL decimal(18,2),
+    VENTA_CODIGO decimal(19,0)
+);  
+GO
+
+CREATE TABLE HARAKIRI.BI_PROVINCIA(
+    PROVINCIA_CODIGO decimal(18,0) not null,
+    PROVINCIA_NOMBRE nvarchar(255)
+);  
+GO
